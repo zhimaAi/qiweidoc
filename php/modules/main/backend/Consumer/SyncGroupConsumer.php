3dd91a453eede61d2aae7e4969431d9726c18019
shellphy <ai.shellphy@gmail.com>
2025-03-27 09:13:54 +0800

    群同步成员数量调整为100

--- php/modules/main/backend/Consumer/SyncGroupConsumer.php
+++ php/modules/main/backend/Consumer/SyncGroupConsumer.php
@@ -29,7 +29,7 @@ class SyncGroupConsumer
     public function handle(): void
     {
         $cursor = "";
-        $limit = 1000;
+        $limit = 100;
 
         $data = [
             "limit" => $limit,
